284f3fa701f541e915d8a7cc03fc449ee6c399d4
Mitchel Wachtel <mitchel.wachtel@gmail.com>
2022-05-17 00:50:57 -0400

    updated contact css

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import "../../styles/Contact.css";
 import gmailPic from "../../assets/images/gmail.png";
+import proPic from "../../assets/images/pro2.jpg";
 
 export default function Contact() {
   const [username, setUsername] = useState("");
@@ -31,63 +32,70 @@ export default function Contact() {
   };
 
   return (
-    <div className="content-container">
-      <div className="contact-header">
-        <div className="contact-top">
-          <h1 className="page-title">Contact</h1>
-          <a
-            href="mailto:mitchel.wachtel@gmail.com"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <img
-              className="linkPic"
-              src={gmailPic}
-              alt="link to Mitchel Wachtel's Email"
-            />
-          </a>
+    <div className="content-container container">
+      <div className="row">
+      <div className="col-6 img-div">
+          <img
+            className="proPic"
+            src={proPic}
+            alt="Professional picture of Mitchel Wachtel"
+          />
+        </div>
+        <div className="col-6">
+          <div className="contact-header">
+            <div className="contact-top">
+              <h1 className="page-title">Contact</h1>
+              <a
+                href="mailto:mitchel.wachtel@gmail.com"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <img
+                  className="linkPic"
+                  src={gmailPic}
+                  alt="link to Mitchel Wachtel's Email"
+                />
+              </a>
+            </div>
+            <h2>mitchel.wachtel@gmail.com</h2>
           </div>
-          <h2>mitchel.wachtel@gmail.com</h2>
-        
-        <div>
-          
+
+          <form className="form">
+            <input
+              className="form-item shadow-none"
+              value={username}
+              name="username"
+              onChange={handleInputChange}
+              type="text"
+              placeholder="Name"
+            />
+            <input
+              className="form-item shadow-none"
+              value={email}
+              name="email"
+              onChange={handleInputChange}
+              type="email"
+              placeholder="Email"
+            />
+            <textarea
+              className="form-item shadow-none"
+              value={message}
+              name="message"
+              onChange={handleInputChange}
+              type="text"
+              placeholder="Message"
+              rows="5"
+            />
+            <button
+              className="form-item submit "
+              type="button"
+              onClick={handleFormSubmit}
+            >
+              Submit
+            </button>
+          </form>
         </div>
       </div>
-
-      <form className="form">
-        <input
-          className="form-item"
-          value={username}
-          name="username"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="Name"
-        />
-        <input
-          className="form-item"
-          value={email}
-          name="email"
-          onChange={handleInputChange}
-          type="email"
-          placeholder="Email"
-        />
-        <textarea
-          className="form-item"
-          value={message}
-          name="message"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="Message"
-          rows="5"
-        />
-        <button
-          className="form-item submit"
-          type="button"
-          onClick={handleFormSubmit}
-        >
-          Submit
-        </button>
-      </form>
     </div>
   );
 }
